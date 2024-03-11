5d8d60be45e596ccc40ad66655a610b42b769a75
sugith10 <sugithkodappurath@gmail.com>
2024-03-11 22:02:00 +0530

    welcome page work in progress

---
+++ lib/core/utils/my_app_exception.dart
@@ -0,0 +1,10 @@
+class MyAppException implements Exception {
+  final String title;
+  final String message;
+  const MyAppException({this.title = "Unexpected", this.message = ""});
+
+  @override
+  String toString() {
+    return 'errror -> title: $title, message: $message';
+  }
+}

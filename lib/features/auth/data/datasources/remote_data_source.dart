780800e9f8f7e1a86b975c21b48b18de3879c470
sugith10 <sugithkodappurath@gmail.com>
2024-03-28 10:06:28 +0530

    going to create android debug key

--- lib/features/auth/data/datasources/remote_data_source.dart
+++ lib/features/auth/data/datasources/remote_data_source.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:take_my_tym/core/utils/app_exception.dart';
 import 'package:take_my_tym/features/auth/data/models/auth_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -113,4 +114,22 @@ class RemoteDataSource {
       );
     }
   }
+
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
 }

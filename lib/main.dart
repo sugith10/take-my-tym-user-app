fc37285f01039325dd931f7925c291568b159a76
sugith10 <sugithkodappurath@gmail.com>
2024-05-03 18:18:24 +0530

    Keys added in env

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:take_my_tym/app.dart';
@@ -11,7 +13,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   DependencyInject().setupDependencies();
+
+  await dotenv.load(fileName: ".env");
 
   final Directory appDocumentDirectory =
       await getApplicationDocumentsDirectory();
@@ -20,6 +25,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  Stripe.publishableKey = dotenv.env["STRIPE_PUBLISH_KEY"]!;
+  await Stripe.instance.applySettings();
+
   Bloc.observer = AppBlocObserver();
+
   runApp(const MyApp());
 }
